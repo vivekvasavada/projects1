3dfa38960ca70e92e0434513c39217a19e10e3b6
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-04 03:53:29 -0400

    added to project

--- Fortuneteller2/Program.cs
+++ Fortuneteller2/Program.cs
@@ -8,173 +8,200 @@ namespace Fortuneteller2
 {
     class Program
     {
-        // private static readonly bool agemsg;
-
-        //string agemsg = null;
-
-        static void Main(string[] args)
-
-
+    }
+    namespace Fortuneteller2
+    {
+        class Program
         {
+            // private static readonly bool agemsg;
+
+            //string agemsg = null;
+
+            static void Main(string[] args)
+
 
             {
 
-                // enter user input
-                Console.WriteLine("Enter your First Name");
-                string FirstName = (Console.ReadLine());
-
-                Console.WriteLine("Enter your Last Name");
-                string LastName = (Console.ReadLine());
-
-                Console.WriteLine("Enter your Age");
-                int age = int.Parse(Console.ReadLine());
-                int retireAge;
-
-                //input odd or even age condition
-                if (age % 2 == 0)
-
                 {
-                    
-                    retireAge = 10;
-                 
 
-                }
-                else
-                {
-                    retireAge = 15;
-                   
-                }
-                //begin of birthmonth logic*******
-                Console.WriteLine("Enter the number of your Birth Month");
-                int birthMonth = int.Parse(Console.ReadLine());
+                    // enter user input
+                    Console.WriteLine("Enter your First Name");
+                    string FirstName = (Console.ReadLine());
 
+                    Console.WriteLine("Enter your Last Name");
+                    string LastName = (Console.ReadLine());
 
-                if (birthMonth == 1 || birthMonth <= 4)
-                {
-                    string bankmsg = ("you have 10,0000 dollars in the bank");
-                    Console.WriteLine(bankmsg);
-                }
-                else if (birthMonth >= 5 && birthMonth <= 8)
+                    Console.WriteLine("Enter your Age");
+                    int age = int.Parse(Console.ReadLine());
+                    int retireAge;
 
-                {
-                    string bankmsg = ("you have 20,000 dollars in the bank");
-                    Console.WriteLine(bankmsg);
+                    //input odd or even age condition
+                    if (age % 2 == 0)
 
-                }
-                else if (birthMonth >= 9 && birthMonth <= 12)
-                {
-                    string bankmsg = ("you have 30,000 dollars in the bank");
-                    Console.WriteLine(bankmsg);
-                }
-                else
-                {
-                    string bankmsg = ("you have zero dollars in the bank");
-                    Console.WriteLine(bankmsg);
-                }
-                //begininng of siblings
-                Console.WriteLine("How many sibilings do you have");
-                int siblingsNumber = int.Parse(Console.ReadLine());
-
-
-                if (siblingsNumber == 0)
-                {
-                    string siblingmsg = ("You will have a vacation home in California");
-                    Console.WriteLine(siblingmsg);
-                }
-                else if (siblingsNumber == 1)
-                {
-                    string siblingmsg = ("You will have a vacation home in Florida");
-                    Console.WriteLine(siblingmsg);
-                }
-                else if (siblingsNumber == 2)
-                {
-                    string siblingmsg = ("You will have a vacation home in Las Vegas");
-                    Console.WriteLine(siblingmsg);
-                }
-                else if (siblingsNumber == 3)
-                {
-                    string siblingmsg = ("You will have a vacation home in Reno");
-                    Console.WriteLine(siblingmsg);
-                }
-
-                else if (siblingsNumber >= 4)
-                {
-                    string siblingmsg = ("You will have a vacation home in Carson City");
-                    Console.WriteLine(siblingmsg);
-
-                }
-
-                else
-                {
-                    string siblingsmsg = ("bad vacation home");
-                    Console.WriteLine(siblingsmsg);
-                }
-                //end of sibling********
-                //end of birthmonthlogic **********
-
-                {
-                    //begin transportation****
-                    Console.WriteLine("What is your favorite ROYGBIV Color or key help for list of colors");
-                    string roygbivColor = (Console.ReadLine().ToUpper());
-                    string transmsg = null;
-                    switch (roygbivColor)
                     {
 
-                        case "RED":
-                            transmsg = "car";
-                            Console.WriteLine(transmsg);
-                            break;
+                        retireAge = 10;
 
 
-
-                        case "ORANGE":
-                            transmsg = "train";
-                            Console.WriteLine(transmsg);
-                            break;
-
+                    }
+                    else
+                    {
+                        retireAge = 15;
 
 
-                        case "YELLOW":
-                            transmsg = "boat";
-                            Console.WriteLine(transmsg);
-                            break;
+                    }
+                    //begin of birthmonth logic*******
+                    Console.WriteLine("Enter the number of your Birth Month");
+                    int birthMonth = int.Parse(Console.ReadLine());
+                    int bankAmt;
 
-                        case "BLUE":
-                            transmsg = "jet";
-                            Console.WriteLine(transmsg);
-                            break;
+                    if (birthMonth == 1 || birthMonth <= 4)
+                    {
+                        bankAmt = 10000;
+                    }
+                    else if (birthMonth >= 5 && birthMonth <= 8)
 
-                        case "GREEN":
-                            transmsg = "motorcycle";
-                            Console.WriteLine(transmsg);
-                            break;
+                    {
+                        bankAmt = 20000;
 
-                        case "VIOLET":
-                            transmsg = "bike";
-                            Console.WriteLine(transmsg);
-                            break;
+                    }
+                    else if (birthMonth >= 9 && birthMonth <= 12)
+                    {
 
-                        case "INDIGO":
-                            transmsg = "scooter";
-                            Console.WriteLine(transmsg);
-                            break;
+                        bankAmt = 30000;
+                    }
+                    else
+                    {
+                        bankAmt = 0;
+                    }
+                    //begininng of siblings
+                    Console.WriteLine("How many sibilings do you have");
+                    int siblingsNumber = int.Parse(Console.ReadLine());
 
-                        case "HELP":
-                            Console.WriteLine("red,orange,yellow,green,blue,indigo,violet");
-                            break;
-                            // End transportation logic******
+
+                    if (siblingsNumber == 0)
+                    {
+                        string siblingmsg = ("You will have a vacation home in California");
+                        Console.WriteLine(siblingmsg);
+                    }
+                    else if (siblingsNumber == 1)
+                    {
+                        string siblingmsg = ("You will have a vacation home in Florida");
+                        Console.WriteLine(siblingmsg);
+                    }
+                    else if (siblingsNumber == 2)
+                    {
+                        string siblingmsg = ("You will have a vacation home in Las Vegas");
+                        Console.WriteLine(siblingmsg);
+                    }
+                    else if (siblingsNumber == 3)
+                    {
+                        string siblingmsg = ("You will have a vacation home in Reno");
+                        Console.WriteLine(siblingmsg);
                     }
 
+                    else if (siblingsNumber >= 4)
+                    {
+                        string siblingmsg = ("You will have a vacation home in Carson City");
+                        Console.WriteLine(siblingmsg);
 
-                    Console.WriteLine(FirstName + " " + LastName + " " + "will retire in" +" " + retireAge + " "+ "years");
-                        
+                    }
+
+                    else
+                    {
+                        string siblingsmsg = ("bad vacation home");
+                        Console.WriteLine(siblingsmsg);
+                    }
+                    //end of sibling********
+
+
+                    {
+                        //  begin transportation****
+                        Console.WriteLine("What is your favorite ROYGBIV Color or key help for list of colors");
+                        string roygbivColor = (Console.ReadLine().ToUpper());
+                        string transmsg = null;
+                        switch (roygbivColor)
+                        {
+
+                            case "RED":
+                                transmsg = "car";
+                                Console.WriteLine(transmsg);
+                                break;
+
+
+
+                            case "ORANGE":
+                                transmsg = "train";
+                                Console.WriteLine(transmsg);
+                                break;
+
+
+
+                            case "YELLOW":
+                                transmsg = "boat";
+                                Console.WriteLine(transmsg);
+                                break;
+
+                            case "BLUE":
+                                transmsg = "jet";
+                                Console.WriteLine(transmsg);
+                                break;
+
+                            case "GREEN":
+                                transmsg = "motorcycle";
+                                Console.WriteLine(transmsg);
+                                break;
+
+                            case "VIOLET":
+                                transmsg = "bike";
+                                Console.WriteLine(transmsg);
+                                break;
+
+                            case "INDIGO":
+                                transmsg = "scooter";
+                                Console.WriteLine(transmsg);
+                                break;
+
+                            case "HELP":
+                                Console.WriteLine("red,orange,yellow,green,blue,indigo,violet");
+                                break;
+                                //     End transportation logic * *****
+                        }
+
+
+                        Console.WriteLine(FirstName + " " + LastName + " " + "will retire in" + " " + retireAge + " " + "years" + " with " + bankAmt + " in the bank" + " ");
+
+
+
+                        //can"t add the string variables "a vacation home in" + vacationLocation and transportaion  
+                    }
+
                 }
 
-            }
 
+            }
 
         }
 
     }
-
 }
+
+    
+
+
+  
+            
+
+            
+
+        
+
+
+    
+
+
+   
+
+
+
+
